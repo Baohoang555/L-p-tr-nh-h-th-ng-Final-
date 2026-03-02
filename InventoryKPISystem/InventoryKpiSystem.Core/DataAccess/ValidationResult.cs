c5285dbebeb343318855a5e5bc30ca7842d7d869
JackStar2 <yellowstarmc@gmail.com>
2026-03-02 19:08:10 +0700

    Models và DataAccess đã xong

---
+++ InventoryKPISystem/InventoryKpiSystem.Core/DataAccess/ValidationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryKpiSystem.Core.DataAccess
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public static ValidationResult Success() => new ValidationResult { IsValid = true };
+        public static ValidationResult Failure(string message) => new ValidationResult { IsValid = false, Message = message };
+    }
+}
