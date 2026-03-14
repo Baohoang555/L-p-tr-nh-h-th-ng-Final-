28403dc7d8a5f91b3bf68bafea7a5e67e4b00b2a
JackStar2 <yellowstarmc@gmail.com>
2026-03-14 20:12:16 +0700

    Sửa lại theo Test Data

--- InventoryKPISystem/InventoryKpiSystem.Core/Models/Product.cs
+++ InventoryKPISystem/InventoryKpiSystem.Core/Models/Product.cs
@@ -3,9 +3,11 @@ namespace InventoryKpiSystem.Core.Models
     public class Product
     {
         public string ItemID { get; set; } = string.Empty;
-        public string Code { get; set; } = string.Empty; 
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-
+        public bool IsSold { get; set; }
+        public bool IsPurchased { get; set; }
         public PriceDetails? PurchaseDetails { get; set; }
         public PriceDetails? SalesDetails { get; set; }
     }
@@ -14,5 +16,6 @@ namespace InventoryKpiSystem.Core.Models
     {
         public decimal UnitPrice { get; set; }
         public string AccountCode { get; set; } = string.Empty;
+        public string TaxType { get; set; } = string.Empty;
     }
 }
