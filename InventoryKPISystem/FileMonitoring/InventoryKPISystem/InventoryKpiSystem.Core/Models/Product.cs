a66e8bf2286ec6d687f9def6f03d867a3e667337
PhanLeTruongAn <phanan2840@gmail.com>
2026-03-14 22:56:42 +0700

    Mem 3 - Bo sung

---
+++ InventoryKPISystem/FileMonitoring/InventoryKPISystem/InventoryKpiSystem.Core/Models/Product.cs
@@ -0,0 +1,9 @@
+namespace InventoryKpiSystem.Core.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+}
